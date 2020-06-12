656e0c2fa17462011a414f91284b699d29a719c9
Juanma <juanma9901@gmail.com>
2020-06-12 15:24:49 -0500

    Incluyendo modelos de Clasificación

--- gamelib/KEYBOARD.py
+++ gamelib/KEYBOARD.py
@@ -1,19 +1,41 @@
 from pygame.locals import *
 import pygame
-# En este documento de aqui se cambian las teclas de movimiento del jugador
-# el pygame.locals es un modulo con un conjunto de variables preestablecidas
+import joblib
+import numpy as np
+import sklearn as sk
+import data
+
+
+model = joblib.load("./gamelib/RandomForest.pkl")
+Xdata = np.loadtxt("./gamelib/Xdata.csv", delimiter=",")
 
 DERECHA = K_RIGHT
 IZQUIERDA = K_LEFT
 SALTO = K_z
 
-def obtener_evento(new_interface = False):
+
+global cuenta       # Cuenta de entradas
+cuenta = [0]        # Lo voli una lista para que la variable no se
+                    # vuelva a resetear al usarla en la función
+
+def obtener_evento(new_interface = True, model=model, data=Xdata,\
+                    cuenta=cuenta):
     if not new_interface:
         return pygame.key.get_pressed()
     else:
-        pass
-                                                            # TO DO
-                                                            # Aqui se configura la nueva intefaz
+        key = pygame.key.get_pressed()
+        KEY = list(key) 
+        if KEY[K_t]:
+            predict = model.predict(Xdata[cuenta[0]].reshape(1,-1))
+            if predict == 1: 
+                KEY[DERECHA] = 1
+                cuenta[0] += 1
+            if predict == 2:
+                KEY[IZQUIERDA] = 1                                               # Aqui se configura la nueva intefaz
+                cuenta[0] += 1 
+        if cuenta[0] == len(Xdata) - 1:
+            cuenta[0] == 0
+        return KEY
                                                             # Ejemplo:
                                                             # if (actividad):
                                                             #     key = pygame.key.get_pressed()
